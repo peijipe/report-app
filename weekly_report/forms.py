94439e11ed27e5c492c5c1709cb362d0855e7088
peijipe <honda@k--s.com>
2019-07-26 07:33:53 +0900

    use forms.Form

--- weekly_report/forms.py
+++ weekly_report/forms.py
@@ -1,11 +1,134 @@
 from django.forms import ModelForm
 from .models import Report
 
-class ReportForm(ModelForm):
-    class Meta:
-        model = Report
-        fields = ['start_date', 'end_date', 'pj_name', 'outline', 'good_point', \
-            'bad_point', 'problem_outline', 'weekly_project_working_hours', \
-            'total_project_working_hours', 'weekly_project_overtime_hours', \
-            'total_project_overtime_hours', 'weekly_internal_working_hours', \
-            'total_internal_working_hours', 'pointed_matter', 'free_text']
+# ModelFormのケース
+# class ReportForm(ModelForm):
+#     class Meta:
+#         model = Report
+#         fields = ['start_date', 'end_date', 'pj_name', 'outline', 'good_point', \
+#             'bad_point', 'problem_outline', 'weekly_project_working_hours', \
+#             'total_project_working_hours', 'weekly_project_overtime_hours', \
+#             'total_project_overtime_hours', 'weekly_internal_working_hours', \
+#             'total_internal_working_hours', 'pointed_matter', 'free_text']
+
+from django import forms
+
+class RepoForm(forms.Form):
+    start_date = forms.DateField(
+        label='開始日',
+        required=True,
+        widget=forms.DateInput(attrs={"type": "date"}),
+        input_formats=['%Y-%m-%d']
+    )
+
+    end_date = forms.DateField(
+        label='終了日',
+        required=True,
+        widget=forms.DateInput(attrs={"type": "date"}),
+        input_formats=['%Y-%m-%d']
+    )
+
+    pj_name = forms.CharField(
+        label='案件名',
+        max_length=50,
+        required=True,
+    )
+
+    outline = forms.CharField(
+        label='タスク一覧',
+        required=True,
+        widget=forms.Textarea(attrs={'rows': 5, 'cols': 100, 'placeholder': 'タスク概要、期限、進捗'})
+    )
+
+    good_point = forms.CharField(
+        label='うまくいった点',
+        required=False,
+        widget=forms.Textarea(attrs={'rows': 5, 'cols': 100, 'placeholder': '概要、理由'})
+    )
+
+    bad_point = forms.CharField(
+        label='うまくいかなかった点',
+        required=False,
+        widget=forms.Textarea(attrs={'rows': 5, 'cols': 100, 'placeholder': '概要、理由、次の一手'})
+    )
+
+    problem_outline = forms.CharField(
+        label='課題',
+        required=False,
+        widget=forms.Textarea(attrs={'rows': 5, 'cols': 100, 'placeholder': '課題概要、課題解決に向けて取り組んでいること、アラートなど'})
+    )
+
+    weekly_project_working_hours = forms.IntegerField(
+        label='週間稼働時間',
+        required=True,
+        min_value=0
+    )
+
+    total_project_working_hours = forms.IntegerField(
+        label='累計稼働時間',
+        required=True,
+        min_value=0
+    )
+
+    def clean_total_project_working_hours(self):
+        weekly_project_working_hours = self.cleaned_data['weekly_project_working_hours']
+        total_project_working_hours = self.cleaned_data['total_project_working_hours']
+
+        if total_project_working_hours < weekly_project_working_hours:
+            raise forms.ValidationError('時間を見直してください。')
+
+        return total_project_working_hours
+    
+    weekly_project_overtime_hours = forms.IntegerField(
+        label='週間残業時間',
+        required=True,
+        min_value=0
+    )
+
+    total_project_overtime_hours = forms.IntegerField(
+        label='累計残業時間',
+        required=True,
+        min_value=0
+    )
+
+    def clean_total_project_overtime_hours(self):
+        weekly_project_overtime_hours = self.cleaned_data['weekly_project_overtime_hours']
+        total_project_overtime_hours = self.cleaned_data['total_project_overtime_hours']
+
+        if total_project_overtime_hours < weekly_project_overtime_hours:
+            raise forms.ValidationError('時間を見直してください。')
+
+        return total_project_overtime_hours
+    
+    weekly_internal_working_hours = forms.IntegerField(
+        label='社内業務 週間残業時間',
+        required=True,
+        min_value=0
+    )
+
+    total_internal_working_hours = forms.IntegerField(
+        label='社内業務 累計残業時間',
+        required=True,
+        min_value=0
+    )
+
+    def clean_total_internal_working_hours(self):
+        weekly_internal_working_hours = self.cleaned_data['weekly_internal_working_hours']
+        total_internal_working_hours = self.cleaned_data['total_internal_working_hours']
+
+        if total_internal_working_hours < weekly_internal_working_hours:
+            raise forms.ValidationError('時間を見直してください。')
+
+        return total_internal_working_hours
+
+    pointed_matter = forms.CharField(
+        label='指摘された点',
+        required=False,
+        widget=forms.Textarea(attrs={'rows': 5, 'cols': 100})
+    )
+
+    free_text = forms.CharField(
+        label='自由欄',
+        required=False,
+        widget=forms.Textarea(attrs={'rows': 5, 'cols': 100})
+    )
--- weekly_report/views.py
+++ weekly_report/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.views.decorators.http import require_POST
-from .forms import ReportForm
+# from .forms import ReportForm　# ModelFormのケース
+from .forms import RepoForm
 
 def index(request):
     users = User.objects.all()
@@ -15,18 +16,66 @@ def users_detail(request, pk):
     reports = user.report_set.all().order_by('-start_date')
     return render(request, 'weekly_report/users_detail.html', {'user': user, 'reports': reports})
 
+# ModelFormのケース
+# def reports_new(request):
+#     if request.method == "POST":
+#         form = ReportForm(request.POST)
+#         if form.is_valid():
+#             report = form.save(commit=False)
+#             report.user = request.user
+#             report.save()
+#             messages.success(request, "報告書を提出しました")
+#         return redirect('weekly_report:users_detail', pk=request.user.pk)
+#     else:
+#         form = ReportForm()
+#     return render(request, 'weekly_report/reports_new.html', {'form': form})
+
 def reports_new(request):
-    if request.method == "POST":
-        form = ReportForm(request.POST)
-        if form.is_valid():
-            report = form.save(commit=False)
-            report.user = request.user
-            report.save()
-            messages.success(request, "報告書を提出しました")
-        return redirect('weekly_report:users_detail', pk=request.user.pk)
-    else:
-        form = ReportForm()
+    form = RepoForm(request.POST or None)
+
+    if form.is_valid():
+        report = Report()
+        report.user = request.user
+        report.start_date = form.cleaned_data['start_date']
+        report.end_date = form.cleaned_data['end_date']
+        report.pj_name = form.cleaned_data['pj_name']
+        report.outline = form.cleaned_data['outline']
+        report.good_point = form.cleaned_data['good_point']
+        report.bad_point = form.cleaned_data['bad_point']
+        report.problem_outline = form.cleaned_data['problem_outline']
+        report.weekly_project_working_hours = form.cleaned_data['weekly_project_working_hours']
+        report.total_project_working_hours = form.cleaned_data['total_project_working_hours']
+        report.weekly_project_overtime_hours = form.cleaned_data['weekly_project_overtime_hours']
+        report.total_project_overtime_hours = form.cleaned_data['total_project_overtime_hours']
+        report.weekly_internal_working_hours = form.cleaned_data['weekly_internal_working_hours']
+        report.total_internal_working_hours = form.cleaned_data['total_internal_working_hours']
+        report.pointed_matter = form.cleaned_data['pointed_matter']
+        report.free_text = form.cleaned_data['free_text']
+    
+        Report.objects.create(
+            user=report.user,
+            start_date=report.start_date,
+            end_date=report.end_date,
+            pj_name=report.pj_name,
+            outline=report.outline,
+            good_point=report.good_point,
+            bad_point=report.bad_point,
+            problem_outline=report.problem_outline,
+            weekly_project_working_hours=report.weekly_project_working_hours,
+            total_project_working_hours=report.total_project_working_hours,
+            weekly_project_overtime_hours=report.weekly_project_overtime_hours,
+            total_project_overtime_hours=report.total_project_overtime_hours,
+            weekly_internal_working_hours=report.weekly_internal_working_hours,
+            total_internal_working_hours=report.total_internal_working_hours,
+            pointed_matter=report.pointed_matter,
+            free_text=report.free_text,
+        )
+        messages.success(request, "報告書を提出しました")
+        
+        return redirect('weekly_report:users_detail', request.user.id)
+
     return render(request, 'weekly_report/reports_new.html', {'form': form})
+
 
 def reports_detail(request, pk):
     report = get_object_or_404(Report, pk=pk)
@@ -38,13 +87,79 @@ def reports_delete(request, pk):
     report.delete()
     return redirect('weekly_report:users_detail', request.user.id)
 
+# ModelFormのケース
+# def reports_edit(request, pk):
+#     report = get_object_or_404(Report, pk=pk)
+#     if request.method == "POST":
+#         form = ReportForm(request.POST, instance=report)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('weekly_report:index')
+#     else:
+#         form = ReportForm(instance=report)
+#     return render(request, 'weekly_report/reports_edit.html', {'form': form, 'report': report})
+
 def reports_edit(request, pk):
     report = get_object_or_404(Report, pk=pk)
+
     if request.method == "POST":
-        form = ReportForm(request.POST, instance=report)
+        form = RepoForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('weekly_report:index')
+            report = Report()
+            report.start_date = form.cleaned_data['start_date']
+            report.end_date = form.cleaned_data['end_date']
+            report.pj_name = form.cleaned_data['pj_name']
+            report.outline = form.cleaned_data['outline']
+            report.good_point = form.cleaned_data['good_point']
+            report.bad_point = form.cleaned_data['bad_point']
+            report.problem_outline = form.cleaned_data['problem_outline']
+            report.weekly_project_working_hours = form.cleaned_data['weekly_project_working_hours']
+            report.total_project_working_hours = form.cleaned_data['total_project_working_hours']
+            report.weekly_project_overtime_hours = form.cleaned_data['weekly_project_overtime_hours']
+            report.total_project_overtime_hours = form.cleaned_data['total_project_overtime_hours']
+            report.weekly_internal_working_hours = form.cleaned_data['weekly_internal_working_hours']
+            report.total_internal_working_hours = form.cleaned_data['total_internal_working_hours']
+            report.pointed_matter = form.cleaned_data['pointed_matter']
+            report.free_text = form.cleaned_data['free_text']
+
+            Report.objects.filter(pk=pk).update(
+                start_date=report.start_date,
+                end_date=report.end_date,
+                pj_name=report.pj_name,
+                outline=report.outline,
+                good_point=report.good_point,
+                bad_point=report.bad_point,
+                problem_outline=report.problem_outline,
+                weekly_project_working_hours=report.weekly_project_working_hours,
+                total_project_working_hours=report.total_project_working_hours,
+                weekly_project_overtime_hours=report.weekly_project_overtime_hours,
+                total_project_overtime_hours=report.total_project_overtime_hours,
+                weekly_internal_working_hours=report.weekly_internal_working_hours,
+                total_internal_working_hours=report.total_internal_working_hours,
+                pointed_matter=report.pointed_matter,
+                free_text=report.free_text
+            )
+
+            report = get_object_or_404(Report, pk=pk)
+            return render(request, 'weekly_report/reports_detail.html', {'report': report, 'pk': pk})
     else:
-        form = ReportForm(instance=report)
+        data = {
+            'start_date': report.start_date,
+            'end_date': report.end_date,
+            'pj_name': report.pj_name,
+            'outline': report.outline,
+            'good_point': report.good_point,
+            'bad_point': report.bad_point,
+            'problem_outline': report.problem_outline,
+            'weekly_project_working_hours': report.weekly_project_working_hours,
+            'total_project_working_hours': report.total_project_working_hours,
+            'weekly_project_overtime_hours': report.weekly_project_overtime_hours,
+            'total_project_overtime_hours': report.total_project_overtime_hours,
+            'weekly_internal_working_hours': report.weekly_internal_working_hours,
+            'total_internal_working_hours': report.total_internal_working_hours,
+            'pointed_matter': report.pointed_matter,
+            'free_text': report.free_text,
+        }
+        form = RepoForm(data)
+        
     return render(request, 'weekly_report/reports_edit.html', {'form': form, 'report': report})
